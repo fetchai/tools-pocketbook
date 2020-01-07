69d37acafdb0d362ea3628e885dc442e953d3a56
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2020-01-07 18:03:31 +0000

    Move pocket book root helper into common folder

--- tests/test_address_book.py
+++ tests/test_address_book.py
@@ -1,30 +1,10 @@
 import os
-import shutil
-import tempfile
 import unittest
 
 import toml
 
 from pocketbook.address_book import AddressBook
-
-
-class TemporaryPocketBookRoot:
-    def __init__(self):
-        self._root = None
-
-    @property
-    def root(self):
-        return self._root
-
-    def __enter__(self):
-        self._root = tempfile.mkdtemp(prefix='pocketbook_', suffix='_root')
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        shutil.rmtree(self._root)
-
-
-SAMPLE_ADDRESS = '2w9uqXLFvCve9TjfoRb2nuztuTx54E1S3AjuB9hWCMMGX5kp4W'
+from .utils import TemporaryPocketBookRoot, SAMPLE_ADDRESS
 
 
 class AddressBookTests(unittest.TestCase):
---
+++ tests/utils.py
@@ -0,0 +1,20 @@
+import shutil
+import tempfile
+
+SAMPLE_ADDRESS = '2w9uqXLFvCve9TjfoRb2nuztuTx54E1S3AjuB9hWCMMGX5kp4W'
+
+
+class TemporaryPocketBookRoot:
+    def __init__(self):
+        self._root = None
+
+    @property
+    def root(self):
+        return self._root
+
+    def __enter__(self):
+        self._root = tempfile.mkdtemp(prefix='pocketbook_', suffix='_root')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        shutil.rmtree(self._root)
