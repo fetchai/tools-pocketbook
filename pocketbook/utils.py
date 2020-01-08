17e6845f3098357851b442b189a3ed90f4bf4d0b
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2020-01-08 10:16:11 +0000

    Unit tests for the utils

--- pocketbook/utils.py
+++ pocketbook/utils.py
@@ -1,21 +1,22 @@
-from fetchai.ledger.api import LedgerApi
 
 
 class NetworkUnavailableError(Exception):
     pass
 
 
-def get_balance(api: LedgerApi, address):
+def get_balance(api, address):
     balance = int(api.tokens.balance(address))
     return balance / 10000000000
 
 
-def get_stake(api: LedgerApi, addresss):
+def get_stake(api, addresss):
     stake = int(api.tokens.stake(addresss))
     return stake / 10000000000
 
 
-def create_api(name: str) -> LedgerApi:
+def create_api(name: str):
+    from fetchai.ledger.api import LedgerApi
+
     try:
         return LedgerApi(network=name)
     except:
